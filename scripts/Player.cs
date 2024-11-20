8502ada0446042b4ceaa55acce5638d1e5d1cec4
Alec Hampton <alhampton9@gmail.com>
2024-11-20 02:29:26 -0700

    Refactoring

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -8,6 +8,8 @@ public partial class Player : Area2D
 	
 	public Vector2 ScreenSize; // Size of the game window.
 
+	private Vector2 _velocity;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -17,41 +19,50 @@ public partial class Player : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
+		SetVelocityAndSpriteBasedOnPlayerInput(delta);
+		SetPositionBasedOnCurrentVelocity(delta);		
+	}
+
+	private void SetVelocityAndSpriteBasedOnPlayerInput(double delta) 
+	{
+		_velocity = Vector2.Zero; // The player's movement vector.
 
 		if (Input.IsActionPressed("move_right"))
 		{
-			velocity.X += 1;
+			_velocity.X += 1;
 		}
 
 		if (Input.IsActionPressed("move_left"))
 		{
-			velocity.X -= 1;
+			_velocity.X -= 1;
 		}
 
 		if (Input.IsActionPressed("move_down"))
 		{
-			velocity.Y += 1;
+			_velocity.Y += 1;
 		}
 
 		if (Input.IsActionPressed("move_up"))
 		{
-			velocity.Y -= 1;
+			_velocity.Y -= 1;
 		}
 
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-		if (velocity.Length() > 0)
+		if (_velocity.Length() > 0)
 		{
-			velocity = velocity.Normalized() * Speed;
+			_velocity = _velocity.Normalized() * Speed;
 			animatedSprite2D.Play();
 		}
 		else
 		{
 			animatedSprite2D.Stop();
 		}
+	}
 
-		Position += velocity * (float)delta;
+	private void SetPositionBasedOnCurrentVelocity(double delta)
+	{
+		Position += _velocity * (float)delta;
 		Position = new Vector2(
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
 			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
