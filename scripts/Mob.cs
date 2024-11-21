4d8ad631e1c50e0004a904f267ceb39653d4dc19
Alec Hampton <alhampton9@gmail.com>
2024-11-21 15:31:35 -0700

    Added mob

---
+++ scripts/Mob.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Mob : RigidBody2D
+{
+	private AnimatedSprite2D _animatedSprite2D;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		SetUpAnimatedSprites();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void SetUpAnimatedSprites()
+	{
+		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	string[] mobTypes = _animatedSprite2D.SpriteFrames.GetAnimationNames();
+	_animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+	}
+}
